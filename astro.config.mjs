44cb234729bbd9ff823e0ee476e8a3836f1c13c3
hiten363 <hiten@binarymetrix.in>
2025-07-23 23:56:16 +0530

    Refactor Astro configuration to use Vite with Tailwind CSS and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from '@astrojs/sitemap';
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hitenkhatri.vercel.app/',
-  integrations: [tailwind(), react(), sitemap()]
+  site: "https://hitenkhatri.vercel.app/",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [react(), sitemap()],
 });
